7ad1219e90101a49c9364c462e88cd84d6349fc4
ruben1421 <rubenantoniomunoz0@gmail.com>
2026-05-04 19:32:27 +0200

    Nuevas Imagenes

--- backend/database/seeders/XuxemonSeeder.php
+++ backend/database/seeders/XuxemonSeeder.php
@@ -24,12 +24,12 @@ class XuxemonSeeder extends Seeder
             ['name' => 'Marea', 'type' => 'Agua', 'size' => 'Pequeño', 'level' => 8, 'attack' => 55, 'defense' => 40],
             ['name' => 'Medusi', 'type' => 'Agua', 'size' => 'Pequeño', 'level' => 15, 'attack' => 50, 'defense' => 75],
             ['name' => 'Patigua', 'type' => 'Agua', 'size' => 'Pequeño', 'level' => 5, 'attack' => 50, 'defense' => 40],
-            ['name' => 'Grisli', 'type' => 'Agua', 'size' => 'Pequeño', 'level' => 5, 'attack' => 40, 'defense' => 50],
-            ['name' => 'Lion', 'type' => 'Agua', 'size' => 'Pequeño', 'level' => 18, 'attack' => 70, 'defense' => 45],
-            ['name' => 'Lodo', 'type' => 'Agua', 'size' => 'Pequeño', 'level' => 10, 'attack' => 45, 'defense' => 60],
-            ['name' => 'Roc', 'type' => 'Agua', 'size' => 'Pequeño', 'level' => 20, 'attack' => 60, 'defense' => 65],
-            ['name' => 'Tortu', 'type' => 'Agua', 'size' => 'Pequeño', 'level' => 8, 'attack' => 55, 'defense' => 40],
-            ['name' => 'Tronc', 'type' => 'Agua', 'size' => 'Pequeño', 'level' => 15, 'attack' => 50, 'defense' => 75],
+            ['name' => 'Grisli', 'type' => 'Tierra', 'size' => 'Pequeño', 'level' => 5, 'attack' => 40, 'defense' => 50],
+            ['name' => 'Lion', 'type' => 'Tierra', 'size' => 'Pequeño', 'level' => 18, 'attack' => 70, 'defense' => 45],
+            ['name' => 'Lodo', 'type' => 'Tierra', 'size' => 'Pequeño', 'level' => 10, 'attack' => 45, 'defense' => 60],
+            ['name' => 'Roc', 'type' => 'Tierra', 'size' => 'Pequeño', 'level' => 20, 'attack' => 60, 'defense' => 65],
+            ['name' => 'Tortu', 'type' => 'Tierra', 'size' => 'Pequeño', 'level' => 8, 'attack' => 55, 'defense' => 40],
+            ['name' => 'Tronc', 'type' => 'Tierra', 'size' => 'Pequeño', 'level' => 15, 'attack' => 50, 'defense' => 75],
             
         ];
 
